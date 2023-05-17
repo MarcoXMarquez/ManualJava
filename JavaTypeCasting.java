ccb0937610a32ea4c7b0730ddf0a4705ca8b1643
Marco <marcomarquezherrera@gmail.com>
2023-05-16 23:42:06 -0500

    second part

---
+++ JavaTypeCasting.java
@@ -0,0 +1,11 @@
+
+public class JavaTypeCasting {
+    public static void mian(String[]args){
+    int myInt = 9;
+    double myDouble = myInt; 
+
+    System.out.println(myInt);      // Output 9
+    System.out.println(myDouble);   // Output 9.0
+
+    }
+a
