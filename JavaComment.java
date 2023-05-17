115f5276c27354fee2257617462c1a3b06a994d6
Marco <marcomarquezherrera@gmail.com>
2023-05-16 23:30:21 -0500

    first part

---
+++ JavaComment.java
@@ -0,0 +1,12 @@
+
+public class JavaComment {
+    public static void main(String[] args) {
+        //imprime Hello World
+        /* imprime Hello World
+        ..
+        .
+        */
+        
+        System.out.println("Hello World");
+    }
+}
